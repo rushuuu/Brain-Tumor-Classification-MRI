dc2b7134943214c67cab9b507de78543eb4e1bae
rushuuu <rushilp4589@gmail.com>
2024-03-24 15:21:37 +0530

    base model prepared

--- src/braintumorclassifier/pipeline/stage_01_data_ingestion.py
+++ src/braintumorclassifier/pipeline/stage_01_data_ingestion.py
@@ -2,7 +2,6 @@ from braintumorclassifier.config.configuration import ConfigurationManager
 from braintumorclassifier.components.data_ingestion import DataIngestion
 from braintumorclassifier import logger
 
-
 STAGE_NAME = "Data Ingestion stage"
 
 class DataIngestionTrainingPipeline:
